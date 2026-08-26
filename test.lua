db590b3c4163a14fc91219473b49d1ca16755f23
jung19354-cpu <jung19354@gmail.com>
2026-08-27 00:55:02 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,6 @@
 print("lade")
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/jokerbiel13/FourHub/refs/heads/main/Doors.lua",true))()
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkolymYT/Scriptss/master/KittyGUI.lua"))()
 
 print("geschafft")
