0703ced9fd0c8531c2a55beccb36fd74ce849d2a
jung19354-cpu <jung19354@gmail.com>
2026-08-27 00:20:55 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@
 print("🔄 Lade dein persönliches Doors-Skript...")
 
 -- 4️⃣ All-in-One Script (Auto-Farm, Revival, etc.)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHunterSolo1/OrangeHub/refs/heads/main/Rehax.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/F2PVN/Breakdoor-script/refs/heads/main/script.lua"))()
 
 -- 5️⃣ Maus-Problem automatisch fixen
 game:GetService("UserInputService").MouseIconEnabled = true
