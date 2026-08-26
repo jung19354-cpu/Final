1b29955dc2063b58688b8858f44878451d3529c5
jung19354-cpu <jung19354@gmail.com>
2026-08-27 00:39:32 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@
 
 print("🔄 Lade dein persönliches Doors-Skript...")
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/zoophiliaphobic/POOPDOORS/main/script.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GIFKITS/gifkits/refs/heads/main/scripts/Auto%20Rooms%20Complete.luau"))()
 
 -- 5️⃣ Maus-Problem automatisch fixen
 game:GetService("UserInputService").MouseIconEnabled = true
