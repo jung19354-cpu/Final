b24556b7baae5d15474157ecb8fd03e2fb55d4fa
jung19354-cpu <jung19354@gmail.com>
2026-08-27 00:52:09 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 print("lade")
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SindubsMini/doors-script/main/Doors/source%20(OrionLib)"))()
 
 
 print("geschafft")
