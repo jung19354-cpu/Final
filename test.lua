dbdb6ec6703409e2079615d8ba0321fab25910ec
jung19354-cpu <jung19354@gmail.com>
2026-08-27 14:34:29 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 print("lade")
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/SindubsMini/doors-script/main/Doors/source%20(OrionLib)')))()
-local Window = OrionLib:MakeWindow({Name = "Doors Script", HidePremium = false, SaveConfig = true, ConfigFolder = "Doors Summon"})
+loadstring(game:HttpGet("https://shadowhub.pro/api/shadow"))()
+
 
 print("geschafft")
