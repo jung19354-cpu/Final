0b6d09ba8729065660d7e9605d4f5a858f7b0b76
jung19354-cpu <jung19354@gmail.com>
2026-08-28 22:51:36 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,8 +7,8 @@ local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. 
 
 -- Optionen für den Save-Vorgang (hier kannst du bei Bedarf etwas anpassen)
 local Options = {
-    -- SafeMode = true, -- Aktiviere diesen Modus, falls es Probleme gibt
-    -- SaveBytecode = true, -- Versuche, Skripte zu dekompilieren (falls unterstützt)
+    SafeMode = true, -- Aktiviere diesen Modus, falls es Probleme gibt
+    SaveBytecode = true, -- Versuche, Skripte zu dekompilieren (falls unterstützt)
 }
 
 -- Führe den Save-Vorgang aus
